f37c943008d5d619bbf49c0297d6eb75140708e0
ssadown <bibatututu@yandex.ru>
2024-09-28 21:46:40 +0500

    Add project files.

---
+++ Tyuiu.MiliukovLO.Sprint3.Task0.V2.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MiliukovLO.Sprint3.Task0.V2.Lib
+{
+    public class DataService : ISprint3Task0V2
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double result = 0;
+            for (int i = startValue; i < stopValue; i++)
+            {
+                result = Math.PI * Math.Pow((i/Math.Pow(Math.Sin(1),-7)),-2);
+            }
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.MiliukovLO.Sprint3.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+using Tyuiu.MiliukovLO.Sprint3.Task0.V2.Lib;
+
+namespace Tyuiu.MiliukovLO.Sprint3.Task0.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService service = new DataService();
+            Assert.AreEqual(1,service.GetMultiplySeries(1, 1));
+        }
+    }
+}
